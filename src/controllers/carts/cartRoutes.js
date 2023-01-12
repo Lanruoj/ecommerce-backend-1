6da3ffbbc79b0927ae2071432d28a17d0eceb251
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-12 13:31:39 +1100

    Populate using virtual(product) in cart

--- src/controllers/carts/cartRoutes.js
+++ src/controllers/carts/cartRoutes.js
@@ -1,32 +1,43 @@
-const express = require("express")
+const express = require("express");
 
-const { getCarts, getCartById, getCartByUserId } = require("./cartControllers")
+const {
+  getCarts,
+  getCartById,
+  getCartByUserId,
+  getCartByUserIdWithProductInfo,
+} = require("./cartControllers");
 
-const cartRouter = express.Router()
+const cartRouter = express.Router();
 
-cartRouter.get("/", (request, response) => {
-    const carts = getCarts()
-    response.json(carts)
-})
+cartRouter.get("/", async (request, response) => {
+  const carts = await getCarts();
+  response.json(carts);
+});
 
-cartRouter.get("/:cartId", (request, response) => {
-    const cart = getCartById(request.params.cartId)
-    if (!cart) {
-        response.status(404).json({
-            data: "cart doesn't exist",
-        })
-    }
-    response.json(cart)
-})
+cartRouter.get("/:cartId", async (request, response) => {
+  const cart = await getCartById(request.params.cartId);
+  if (!cart) {
+    response.status(404).json({
+      data: "cart doesn't exist",
+    });
+  }
+  response.json(cart);
+});
 
-cartRouter.get("/user/:userId", (request, response) => {
-    const cart = getCartByUserId(request.params.userId)
-    if (!cart) {
-        response.status(404).json({
-            data: "cart doesn't exist",
-        })
-    }
-    response.json(cart)
-})
+cartRouter.get("/user/:userId", async (request, response) => {
+  let cart;
+  if (request.query.getProductInfo) {
+    cart = await getCartByUserIdWithProductInfo(request.params.userId);
+  } else {
+    cart = await getCartByUserId(request.params.userId);
+  }
 
-module.exports = cartRouter
+  if (!cart) {
+    response.status(404).json({
+      data: "cart doesn't exist",
+    });
+  }
+  response.json(cart);
+});
+
+module.exports = cartRouter;
